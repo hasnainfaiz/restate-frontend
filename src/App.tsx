cdbe7386e8adf3f4e40c5090064f48989b0ee397
hasnain <muhammad.hasnain@phaedrasolutions.com>
2023-08-11 16:42:17 +0500

    new changes into master

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-// import { auth } from "./services/utils/firebase";
-// import { GoogleAuthProvider } from 'firebase/auth';
-// import { socialLoginWithPopUp } from './services/utils/firebase-login';
-// import { images } from './assets';
+import { auth } from "./services/utils/firebase";
+import { GoogleAuthProvider } from 'firebase/auth';
+import { socialLoginWithPopUp } from './services/utils/firebase-login';
+import { images } from './assets';
 import './App.scss';
 
 function App() {
-  // const googleLogin = async () => {
-  //   const provider = GoogleAuthProvider;
-  //   const user = await socialLoginWithPopUp(auth, provider,);
-  //   console.log(user, "user is this one")
-  // }
+  const googleLogin = async () => {
+    const provider = GoogleAuthProvider;
+    const user = await socialLoginWithPopUp(auth, provider,);
+    console.log(user, "user is this one")
+  }
   return (
     <div>
       <div className="App">
@@ -20,12 +20,12 @@ function App() {
       <div>
         <p className='test'>Testing</p>
       </div>
-      {/* <div>
+      <div>
         <button onClick={googleLogin}>Test Login</button>
       </div>
       <div>
         <img src={images.cclogo} height={90} width={90} alt='testing' />
-      </div> */}
+      </div>
     </div>
   );
 }
